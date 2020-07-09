003218fbb5bd01089b83551cb6647bdaf92c2b5c
pilets <yauheni.pilets@dynevo.org>
2020-07-09 19:15:04 +0300

    init commit

---
+++ functions.php
@@ -0,0 +1,171 @@
+<?php 
+
+
+define('B_THEME_PATH', get_template_directory_uri(  ));
+define('B_CSS_PATH', B_THEME_PATH . '/build/css');
+define('B_JS_PATH', B_THEME_PATH . '/build/js');
+
+
+//подключение стилей и скриптов
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style('bootstrap', B_CSS_PATH . '/bootstrap.min.css');
+    wp_enqueue_style('control-style', B_CSS_PATH . '/main.css');
+    wp_deregister_script( 'jquery' );
+    wp_enqueue_script('jquery', B_JS_PATH . '/jquery.js');
+    wp_enqueue_script('slick', B_JS_PATH . '/slick.min.js');
+    wp_enqueue_script('main', B_JS_PATH . '/main.js');
+});
+
+
+add_action( 'after_setup_theme', 'alm_remove_image_size', 1 );
+add_action( 'init', 'register_post_types' );
+
+//регтсрация новых полей
+function register_post_types(){
+	add_theme_support( 'post-thumbnails'); 
+
+	//примеры 
+	register_post_type( 'example', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Примеры', // основное название для типа записи
+			'singular_name'      => 'Примеры', // название для одной записи этого типа
+			'add_new'            => 'Добавить пример', // для добавления новой записи
+			'add_new_item'       => 'Добавление нового примера', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование примера', // для редактирования типа записи
+			'new_item'           => 'Текст примера', // текст новой записи
+			'view_item'          => 'Смотреть примеры', // для просмотра записи этого типа.
+			'search_items'       => 'Искать примеры', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Примеры', // название меню
+		],
+		'public'              => false,
+		'show_ui'             => true, 
+		'menu_icon'           => null,
+		'supports'            => [ 'title' , 'thumbnail'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+	] );
+
+	// отзывы	
+	register_post_type( 'reviews', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Отзывы', // основное название для типа записи
+			'singular_name'      => 'Отзывы', // название для одной записи этого типа
+			'add_new'            => 'Добавить отзыв', // для добавления новой записи
+			'add_new_item'       => 'Добавление новый отзыв', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование отзыва', // для редактирования типа записи
+			'new_item'           => 'Новое ____', // текст новой записи
+			'view_item'          => 'Смотреть отзывы', // для просмотра записи этого типа.
+			'search_items'       => 'Искать отзывы', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Отзывы', // название меню
+		],
+		'public'              => false,
+		'show_ui'             => true, 
+		'menu_icon'           => null,
+		'supports'            => [ 'title' , 'thumbnail'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+	]);
+
+	//main
+	register_post_type( 'static', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Установочные данные', // основное название для типа записи
+			'singular_name'      => 'Установочные данные', // название для одной записи этого типа
+			'add_new'            => 'Добавить Установочные данные', // для добавления новой записи
+			'add_new_item'       => 'Добавление новый Установочные данные', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование Установочные данные', // для редактирования типа записи
+			'new_item'           => 'Новое ____', // текст новой записи
+			'view_item'          => 'Смотреть Установочные данные', // для просмотра записи этого типа.
+			'search_items'       => 'Искать Установочные данные', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Установочные данные', // название меню
+		],
+		'public'              => false,
+		'show_ui'             => true, 
+		'menu_icon'           => null,
+		'supports'            => ['thumbnail'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+	]);
+}
+
+//размер загружаеых изображений
+function alm_remove_image_size() {
+	global $ajax_load_more;
+	remove_filter( 'after_setup_theme', array( $ajax_load_more, 'alm_image_sizes' ) );
+}
+ 
+
+//получение отзывов
+ function getReviews() {
+    $args = array(
+		'numberposts' => '30',
+        'orderby' => 'date',
+        'order' => 'ASC', 
+        'post_type' => 'reviews',
+    );
+
+	$reviews = [];
+
+    foreach(get_posts($args) as $post) {
+		$review = get_fields($post->ID);
+		$review['name'] = $post->post_title;
+		$review['img'] = get_the_post_thumbnail_url( $post->ID, 'thumbnail' );
+		$reviews[] = $review;
+    }
+    
+    return $reviews;
+}
+
+
+//получение готовых работ
+function getExamples() {
+    $args = array(
+		'numberposts' => '30',
+        'orderby' => 'date',
+        'order' => 'ASC', 
+        'post_type' => 'example',
+    );
+
+	$examples = [];
+
+    foreach(get_posts($args) as $post) {
+		$example = get_fields($post->ID);
+		$example['name'] = $post->post_title;
+		$example['img'] = get_the_post_thumbnail_url( $post->ID, 'thumbnail' );
+		$examples[] = $example;
+    }
+    
+    return $examples;
+}
+
+
+function getMain() {
+    $args = array(
+		'numberposts' => '30',
+        'orderby' => 'date',
+        'order' => 'ASC', 
+        'post_type' => 'static',
+    );
+
+	$main = [];
+
+    foreach(get_posts($args) as $post) {
+		$main = get_fields($post->ID);
+		$main['img'] = get_the_post_thumbnail_url( $post->ID, 'thumbnail' );
+		$main[] = $example;
+    }
+    
+    return $main;
+}
+
+
+
+
+
+
---
+++ src/js/main.js
@@ -0,0 +1,164 @@
+
+function initSliders() {
+    $(document).ready(function(){
+        //hero block slider
+        
+        $('.hero-content-slider').slick({
+            arrows: true,
+            slidesToShow: 2,
+            prevArrow: document.querySelector('.hero-content-slider-buttons-prev'),
+            nextArrow: document.querySelector('.hero-content-slider-buttons-next'),
+            infinite: false,
+            }
+        );
+        $('.hero-content-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+            console.log(currentSlide);
+            console.log(slick.slideCount)
+            if(currentSlide !== 0) {
+                document.querySelector('.hero-content-slider-buttons-prev').classList.add('active')
+            } else {
+                document.querySelector('.hero-content-slider-buttons-prev').classList.remove('active')
+            }
+            if(currentSlide === slick.slideCount - 2) {
+                document.querySelector('.hero-content-slider-buttons-next').classList.remove('active')
+            } else {
+                document.querySelector('.hero-content-slider-buttons-next').classList.add('active')
+            }
+        });
+        //offer block slider
+        $('.offer-slider').slick({
+            arrows: false,
+            slidesToShow: 1,
+            speed: 5000,
+            autoplay: true,
+            fade: true,
+            }
+        );
+
+        $('.example-slider').slick({
+            arrows: true,
+            slidesToShow: 3,
+            infinite: false,
+            centerMode: true,
+            prevArrow: document.querySelector('.example-slider-buttons-prev'),
+            nextArrow: document.querySelector('.example-slider-buttons-next'),
+            }
+        );
+        $('.example-slider').slick('slickGoTo', 2,  true);
+
+        $('.comment-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true,
+            prevArrow: document.querySelector('.comment-slider-arrows-prev'),
+            nextArrow: document.querySelector('.comment-slider-arrows-next'),
+            }
+        );
+    });
+}
+
+
+window.addEventListener('load', function () {
+    siteNavigation();
+    
+});
+
+
+
+function siteNavigation() {
+    let count = 0;
+    let newCount;
+    let inProgress = false;
+    let sections = Array.prototype.slice.call(document.querySelectorAll('section'));
+    let maxCount = sections.length - 1;
+    let changeCountEvent = new Event('changeCount');
+    let menuSection = document.querySelector('.menu');
+    let menuItems = Array.prototype.slice.call(document.querySelectorAll('.menu-item'));
+
+
+
+    menuItems.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            count = item.getAttribute('data-num');
+            window.dispatchEvent(changeCountEvent);
+            menuSection.classList.remove('open')
+        })
+    })
+
+    window.addEventListener('changeCount', (e) => {
+        renderPage(count)
+    })
+
+    function renderPage (num) {
+        sections[num].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+    }
+
+    function changeCountToWhell () {
+        window.addEventListener('wheel', (e) => {
+            if(!inProgress) {
+                if(e.deltaY > 0) {
+                    (count < maxCount) ? count++ : count = maxCount;
+                    window.dispatchEvent(changeCountEvent)
+                    inProgress = true;
+                    changeProgress()
+                } else if (e.deltaY < 0) {
+                    (count > 0 && count <= maxCount) ? count-- : count = 0;
+                    window.dispatchEvent(changeCountEvent)
+                    inProgress = true;
+                    changeProgress()
+                }
+            }
+        })
+        document.querySelector('.body-wrapper').addEventListener('wheel', (e) => {
+            e.preventDefault();
+        })
+    }
+
+    function changeCountToClick() {
+        let buttonsTop = Array.prototype.slice.call(document.querySelectorAll('.grid-counter-top'));
+        let buttonsBottom = Array.prototype.slice.call(document.querySelectorAll('.grid-counter-bottom'));
+        buttonsTop.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                (count > 0 && count <= maxCount) ? count-- : count = 0;
+                window.dispatchEvent(changeCountEvent)
+            })
+        })
+        buttonsBottom.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                (count < maxCount) ? count++ : count = maxCount;
+                window.dispatchEvent(changeCountEvent)
+            })
+        })
+    }
+
+    function changeProgress () {
+        setTimeout(function() {
+            inProgress = false
+        }, 1000)
+    }
+
+    function menuBehavior () {
+        let menuOpenButton = document.querySelectorAll('.hamburger');
+        menuOpenButton.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                menuSection.classList.toggle('open')
+            })
+        })
+    
+    }
+
+
+    menuBehavior()
+    renderPage(0)
+    changeCountToWhell()
+    changeCountToClick()
+    
+}
+
+
+
+initSliders()
+
